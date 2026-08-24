383db523e5c9c207325e01fe8f01c2f40e0a121d
Rishi Yadav <rishi.ry7@gmail.com>
2026-08-25 00:04:54 +0530

    postgres update

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,6 +11,12 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/internship_db")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Automatically create the database if it doesn't exist
+if not database_exists(engine.url):
+    print("Database does not exist. Creating it automatically...")
+    create_database(engine.url)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
